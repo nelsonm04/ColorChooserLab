60577cba195c4c76a48a00a08d7714b15046399a
Nelson Mojica <nelsonm43@icloud.com>
2025-03-04 18:00:17 -0500

    changes

--- src/main/java/com/example/colorchooserlab/ColorChooser.java
+++ src/main/java/com/example/colorchooserlab/ColorChooser.java
@@ -2,22 +2,23 @@ package com.example.colorchooserlab;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-
-public class HelloApplication extends Application {
+public class ColorChooser extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+    public void start(Stage stage) throws Exception {
+        Parent root =
+                FXMLLoader.load(getClass().getResource("ColorChooser.fxml"));
+
+        Scene scene = new Scene(root);
+        stage.setTitle("Color Chooser");
         stage.setScene(scene);
         stage.show();
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 }
--- src/main/java/com/example/colorchooserlab/ColorChooserController.java
+++ src/main/java/com/example/colorchooserlab/ColorChooserController.java
@@ -1,14 +1,51 @@
-package com.example.colorchooserlab;
-
+package com.example.colorchooserlab;// Fig. 13.9: ColorChooserController.java
+// Controller for the ColorChooser app
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+import javafx.scene.control.TextField;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.util.converter.NumberStringConverter;
 
-public class HelloController {
-    @FXML
-    private Label welcomeText;
+public class ColorChooserController {
+    // instance variables for interacting with GUI components
+    @FXML private Slider redSlider;
+    @FXML private Slider greenSlider;
+    @FXML private Slider blueSlider;
+    @FXML private Slider alphaSlider;
+    @FXML private TextField redTextField;
+    @FXML private TextField greenTextField;
+    @FXML private TextField blueTextField;
+    @FXML private TextField alphaTextField;
+    @FXML private Rectangle colorRectangle;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    // instance variables for managing
+    private int red = 0;
+    private int green = 0;
+    private int blue = 0;
+    private double alpha = 1.0;
+
+    public void initialize() {
+        redTextField.textProperty().bindBidirectional(redSlider.valueProperty(), new NumberStringConverter());
+        greenTextField.textProperty().bindBidirectional(greenSlider.valueProperty(), new NumberStringConverter());
+        blueTextField.textProperty().bindBidirectional(blueSlider.valueProperty(), new NumberStringConverter());
+        alphaTextField.textProperty().bindBidirectional(alphaSlider.valueProperty(), new NumberStringConverter());
+
+        redSlider.valueProperty().addListener((obs, oldVal, newVal) -> updateColor());
+        greenSlider.valueProperty().addListener((obs, oldVal, newVal) -> updateColor());
+        blueSlider.valueProperty().addListener((obs, oldVal, newVal) -> updateColor());
+        alphaSlider.valueProperty().addListener((obs, oldVal, newVal) -> updateColor());
+    }
+
+    // Method to update the rectangle's color
+    private void updateColor() {
+        red = (int) redSlider.getValue();
+        green = (int) greenSlider.getValue();
+        blue = (int) blueSlider.getValue();
+        alpha = alphaSlider.getValue();
+
+        colorRectangle.setFill(Color.rgb(red, green, blue, alpha));
     }
 }
